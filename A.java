b9e59b100b347423b6e03ec7e6146176977cc73a
sagisk <tone.menu@mail.ru>
2025-03-13 19:52:36 +0400

    deleted redundant A.java

--- A.java
+++
@@ -1,7 +0,0 @@
-## Setup
-**GitHub Personal Access Token**: Create a GitHub personal access token with repository access by following GitHub's guide. Replace the placeholder "your_token" with your token.
-        **GitHub Repository**: Specify your GitHub repository details (owner, repo, branch) and the file (filePath) you want to commit.
-
-Dependencies: Add the necessary dependencies to your build.sbt.
-
-File Path: Update filePath with the file you want to commit (e.g., "path_to_the_dag").
